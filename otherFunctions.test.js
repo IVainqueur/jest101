97b49f3c2b1eff27932c0912df91fd869cb9319e
IVainqueur <ishimvainqueur@gmail.com>
2022-08-12 23:26:17 +0200

    auto-commit-19cd53dd595a4b3e8a9274f564557f93

--- otherFunctions.test.js
+++ otherFunctions.test.js
@@ -2,5 +2,7 @@ const otherFunctions = require('./otherFunctions')
 
 /* 
 The .toBe matcher does not work as expected on objects and arrays because it is strict 
-and reference types (arrays and objects) also compare 
+and reference types (arrays and objects) also compare memory and shit 
+so...........
+the .toEqual matcher is usually used
 */
