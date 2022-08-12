c5a9970b34582e4e84af8f748a7e386aa4fc5b96
IVainqueur <ishimvainqueur@gmail.com>
2022-08-13 00:00:26 +0200

    auto-commit-be89bc80deaf44be954e48337da889a3

--- cloneArray.test.js
+++ cloneArray.test.js
@@ -1,10 +1,10 @@
 const cloneArray = require('./cloneArray')
 
 /* This won't work */
-test('Proper Cloning', ()=>{
-    const arr = [1, 2, 3]
-    expect(cloneArray(arr)).toBe(arr)
-})
+// test('Proper Cloning', ()=>{
+//     const arr = [1, 2, 3]
+//     expect(cloneArray(arr)).toBe(arr)
+// })
 
 /* This will work */
 test('Proper Cloning', ()=>{
