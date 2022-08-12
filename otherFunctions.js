a8102803c1c7fe5c52e32b082c5af08f6e2f5563
IVainqueur <ishimvainqueur@gmail.com>
2022-08-12 23:54:30 +0200

    auto-commit-469d2152f54941259c373a62a4f160c3

--- otherFunctions.js
+++ otherFunctions.js
@@ -8,7 +8,8 @@ const otherFunctions = {
         }
     },
     fetchUser: async ()=>{
-        axios.get('')
+       let user =  axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.data)
     }
 }
 
